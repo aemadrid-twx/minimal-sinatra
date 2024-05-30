2771ec8647338b81b283597754c562f7b75aa084
Adrian Madrid <adrian.madrid@timeworxs.com>
2024-05-30 16:14:11 -0600

    Better commit pull

--- app.rb
+++ app.rb
@@ -2,18 +2,19 @@
 require 'sinatra'
 require 'sinatra/json'
 
-begin
-  REVISION = `git rev-parse --short HEAD`.chomp
-rescue => e
-  REVISION = format 'ERROR: %s : %s', e.class.name, e.message
-end
+REVISION = ENV.fetch('SOURCE_COMMIT') { `git rev-parse --short HEAD`.chomp }
+ENV_PASSWORD = ENV.fetch('ENV_PASSWORD') { '1234' }
 
 get '/' do
   json status: 'ok', revision: REVISION
 end
 
 get '/env' do
-  env_vars = ENV.keys.sort.each_with_object({}) { |k, h| h[k] = ENV[k] }
+  env_vars = if params[:pass] == ENV_PASSWORD
+               ENV.keys.sort.each_with_object({}) { |k, h| h[k] = ENV[k] }
+             else
+               {}
+             end
   json status: 'ok', env: env_vars
 end
 
