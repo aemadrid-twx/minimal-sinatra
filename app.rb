2343cfb89ca7425f4fce9622b6626f3443490b54
Adrian Madrid <adrian.madrid@timeworxs.com>
2024-05-30 16:21:22 -0600

    refactoring

--- app.rb
+++ app.rb
@@ -6,16 +6,28 @@ REVISION = ENV.fetch('SOURCE_COMMIT') { `git rev-parse --short HEAD`.chomp }
 ENV_PASSWORD = ENV.fetch('ENV_PASSWORD') { '1234' }
 
 get '/' do
-  json status: 'ok', revision: REVISION
+  json status: 'ok'
+end
+
+get '/info' do
+  json url: ENV.fetch('COOLIFY_URL') { 'missing' },
+       fqdn: ENV.fetch('COOLIFY_FQDN') { 'missing' },
+       host: ENV.fetch('HOST') { 'missing' },
+       port: ENV.fetch('PORT') { 'missing' },
+       branch: ENV.fetch('COOLIFY_BRANCH') { 'missing' },
+       revision: ENV.fetch('SOURCE_COMMIT') { 'missing' }
 end
 
 get '/env' do
-  env_vars = if params[:pass] == ENV_PASSWORD
-               ENV.keys.sort.each_with_object({}) { |k, h| h[k] = ENV[k] }
-             else
-               {}
-             end
-  json status: 'ok', env: env_vars
+  if params[:pass] == ENV_PASSWORD
+    envs = ENV.keys.sort.each_with_object({}) do |k, h|
+      h[k] = ENV[k]
+    end
+  else
+    envs = {}
+  ends
+
+  json status: 'ok', env: envs
 end
 
 get '/status' do
